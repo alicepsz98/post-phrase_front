412c20058a170af94656d3a128f93b61e3607302
Alice Souza <alicepsouzaba1998@gmail.com>
2022-02-13 22:19:43 -0300

    initial implementations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      Testando...
-    </div>
+    <>
+      <Header />
+      <Home />
+      <Footer />
+      <GlobalStyles />
+    </>
   );
 }
 
