b58c8eef80788975746d95f75b1e9e90fb9ad388
Alice Souza <alicepsouzaba1998@gmail.com>
2022-02-16 21:54:55 -0300

    post card

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,19 +1,28 @@
 import React from "react";
 import * as Style from "./styles";
+import moment from "moment";
 
 export const PostCard = (props) => {
   return (
-    <Style.Container>
-      {props.data?.map((phrase) => {
-        return (
-          <div key={phrase.id}>
-            <h3>{phrase.posted_to}</h3>
-            <p>"{phrase.phrase}"</p>
-            <h4>{phrase.author}</h4>
-            <h4>{phrase.created_at}</h4>
-          </div>
-        );
-      })}
-    </Style.Container>
+    <>
+      {props.data &&
+        props.data
+          ?.map((phrase) => {
+            return (
+              <Style.Container key={phrase.id}>
+                <h3>{phrase.posted_to}</h3>
+                <p>"{phrase.phrase}"</p>
+                {phrase.author && (
+                  <div>
+                    <h4>Autor(a):</h4>{" "}
+                    <h4 className="authorName">{phrase.author}</h4>
+                  </div>
+                )}
+                <h5>{moment(phrase.created_at).format("DD/MM/YYYY, HH:mm")}</h5>
+              </Style.Container>
+            );
+          })
+          .reverse()}
+    </>
   );
 };
--- src/components/PostCard/styles.js
+++ src/components/PostCard/styles.js
@@ -1,7 +1,34 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div` 
-  width: 300px;
-  padding: 5px;
-  background: white;
+  padding: 15px;
+  background: #005428;
+  color: white;
+  font-size: 18px;
+  width: 48.7%;
+  color: #3ffc99;
+  text-align: left;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 15px;
+  div {
+    display: flex;
+    gap: 10px;
+  }
+  h3, .authorName {
+    text-transform: capitalize;
+  }
+  p {
+    color: white;
+  }
+  h5 {
+    text-align: right;
+    font-size: 16px;
+    font-weight: 300;
+  }
+  @media (max-width: 826px) {
+    width: 100%;
+  }
 `;
